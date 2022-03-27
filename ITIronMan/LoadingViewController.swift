4d458962bad609bfe820174cc221be0fe8b160ff
yuHsuan <x89301089@gmail.com>
2022-03-27 16:57:42 +0800

    [add] LoadingViewController AlamofireAdapter test

--- ITIronMan/LoadingViewController.swift
+++ ITIronMan/LoadingViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class LoadingViewController: UIViewController {
+    
+    private lazy var alamofireAdapter: AlamofireAdapter = {
+        return AlamofireAdapter()
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        requestPlaceholderAPI()
     }
-
-
+    
+    private func requestPlaceholderAPI() {
+        
+        let urlString = "https://jsonplaceholder.cypress.io/todos/1"
+        
+        alamofireAdapter.request(urlString, method: .get) { data, response, error in
+            
+            if let error = error {
+                print("you got error: \(error.localizedDescription)")
+                return
+            }
+            
+            if let data = data {
+                print("you got data: \(String(describing: String(data: data, encoding: .utf8)))")
+            }
+        }
+    }
 }
-
